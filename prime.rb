c22ee34b62de09e40ab2b1502dc1ebfa7a79d949
Marsha Annon <marshaannon@gmail.com>
2020-03-07 16:28:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@ def prime?(num)
   range = 0..num-1
     range.each do |x|
 
-      if if num % x == 0
-        return false
-      else
-        true
+     if num % x == 0
+       return false
+    else
+       true
       end
     end
 end
