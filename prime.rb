60729004439bdcda50b593b1666fec77acd296e6
Marsha Annon <marshaannon@gmail.com>
2020-03-07 17:09:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(num)
   range = (2..num-1)
     if num < 0
-    return false
-    elsif range.any? {|x| num % x == 0}
-      return true
-    else
       return false
+    elsif range.any? {|x| num % x == 0}
+      return false
+    else
+      return true
     end
 
 end
